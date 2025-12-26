4ec96a535587dbc998cba31aa8deccb974e007be
Cr117S <chethanasiriwardena@gmail.com>
2025-12-26 17:11:24 +0530

    nav bar fix part 1

--- Public/Scripts/home.js
+++ Public/Scripts/home.js
@@ -1,3 +1,4 @@
+// slider
 const track = document.getElementById("track");
 const slides = track.querySelectorAll(".slide");
 let index = 0;
@@ -7,6 +8,7 @@ setInterval(() => {
   track.style.transform = `translateX(-${index * 100}%)`;
 }, 3500);
 
+// author selector
 const authors = [
   "James Clear",
   "George Orwell",
@@ -19,7 +21,7 @@ const authors = [
 
 // Make a stable "day number" (changes daily)
 const today = new Date();
-const dayNumber = Math.floor(today.getTime() / (1000 * 60 * 60 * 24)); // days since 1970
+const dayNumber = Math.floor(today.getTime() / (1000 * 60 * 60 * 24));
 
 // Pick author for today
 const i = dayNumber % authors.length;
